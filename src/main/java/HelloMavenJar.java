181824381af1bb1b5fbec4ae69e4aa457ef5e46c
almiraj <rain.almiraj@gmail.com>
2023-06-13 11:14:26 +0900

    first commit

---
+++ src/main/java/HelloMavenJar.java
@@ -0,0 +1,11 @@
+
+// [console] mvn package
+// [eclipse] Run As -> Maven Build -> Goals:package -> Run
+//   -> packaged to "target/HelloMavenJar-0.0.1-SNAPSHOT.jar"
+public class HelloMavenJar {
+
+	public static void main(String[] args) {
+		System.out.println("Hello maven jar!");
+	}
+
+}
